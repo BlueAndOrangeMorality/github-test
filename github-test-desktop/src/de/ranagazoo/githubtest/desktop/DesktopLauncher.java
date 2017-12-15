032e3c9a893d76f2d0840d1e974b7327a34aad6b
Kai Köpke <kai.koepke@quinscape.de>
2017-12-15 15:02:56 +0100

    Test

---
+++ github-test-desktop/src/de/ranagazoo/githubtest/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package de.ranagazoo.githubtest.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import de.ranagazoo.githubtest.GithubTestGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GithubTestGame(), config);
+	}
+}
